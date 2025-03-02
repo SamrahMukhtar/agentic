4b0c3746a12f5e065d76a4e54f2d2d103c23dfd3
SamrahMukhtar <samrah.mukhtar@gmail.com>
2025-03-02 15:21:27 +0500

    Update main.py
    Changes in main

--- src/agentic/main.py
+++ src/agentic/main.py
@@ -7,7 +7,7 @@ st.title("My First Streamlit App")
 st.write("Hello from my First Streamlit App!")
 st.text("This is a text")
 # image = st.camera_input("Take a picture")
-if image:
+"""'if image:
     st.image(image)
-
+"""
 st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
